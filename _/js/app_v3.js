db13e867c1824b1f135a16de9f122fe0af69509b
noplanalderson <noplanalderson@gmail.com>
2021-10-02 21:13:56 +0700

    Add code description in Bahasa Indonesia

--- _/js/app_v3.js
+++ _/js/app_v3.js
@@ -15,9 +15,16 @@
  * 
 */
 
+    // Konstanta untuk Versi Dashboard
     const VERSION = 'v1.4.1';
 
-    // Initialize Datetime Range Picker
+    /**
+     *
+     * Menginisialiasi plugin daterange picker
+     * Aktifkan mode timepicker
+     * Gunakan format YYYY-MM-DD HH:mm:ss
+     *
+    */
     $('#range').daterangepicker({
         timePicker: true,
 
@@ -27,7 +34,14 @@
         }
     });
 
-    // Firebase Configuration
+    //-----------------------------------------------------------------
+    
+    /**
+     *
+     * Parameter untuk konfigurasi google firebase
+     * Konfigurasi ini bisa didapatkan pada firebase console
+     * 
+    */
     var firebaseConfig = {
         apiKey: "AIzaSyBOOCVHszEFrz1Zfztf3jeQkRdkIl7KdGo",
         authDomain: "gasleak-d2ef7.firebaseapp.com",
@@ -39,26 +53,51 @@
         measurementId: "G-HN6ZYE5NB0"
     };
 
-    // Initialize Firebase
+    //------------------------------------------------------------------
+    
+    /**
+     *
+     * Menginisialisasi konfigurasi dan koneksi firebase
+     * Mengaktifkan google analytics untuk firebase
+     *  
+    */
     firebase.initializeApp(firebaseConfig);
     firebase.analytics();
 
-    // Initialize Database Utility
+    //------------------------------------------------------------------
+    
+
+    /**
+     * 
+     * Memanggil database pada firebase
+     * 
+    */
     var database = firebase.database();
 
+    //------------------------------------------------------------------
+    
+    // Variable default untuk data berdasarkan rentang waktu
     var minDate, maxDate;
 
+    // Variable default untuk menampung array yang berisi lokasi sensor
     var sensorLocations = [];
 
+    // Variable default untuk menampung data sensor yang didapat dari firebase
     var tableData = [];
 
+    // Variable untuk datatable
     var table;
 
+    // String untuk keterangan periode/rentang waktu yang dipilih
     var period = '';
 
-    // Initialize Datatables
+    // Inisialisasi plugin Datatables dengan konfigurasi default
     $('#sensor_data_tbl').DataTable();
 
+    /**
+     * Fungsi untuk menonaktifkan alarm
+     * 
+    */
     function audioDown(id) {
       var x = document.createElement("AUDIO");
 
@@ -74,6 +113,12 @@
       document.body.appendChild(x);
     }
 
+    //------------------------------------------------------------------
+    
+    /**
+     * Fungsi untuk mengaktifkan alarm
+     * 
+    */
     function audioUp(id) {
       var x = document.createElement("AUDIO");
 
@@ -89,50 +134,75 @@
       document.body.appendChild(x);
     }
 
-    // Get Sensors
+    //------------------------------------------------------------------
+    
+
+    /**
+     * 
+     * Mengambil nama sensor dari firebase untuk ditampilkan menjadi
+     * dropdown menu
+     * 
+    */
     database.ref('/').on('value', function (snapshot) {
 
         var value = snapshot.val();
-        
-        var sensorNames = [];
 
         var option = '';
 
+        // Default option
         option += '<option value="" data-location="">Choose Sensor</option>';
-        
-        // Get Sensor Data from Every Sensor Name 
-        $.each(value, function (index, value) {
-            
+
+        // Buat daftar option untuk setiap sensor 
+        $.each(value, function (index, value) {    
             
             option += '<option value="'+index+'" data-location="'+value.sensor_location+'">'+index+'</option>';
         });
         
+        // Tampilkan opsi sensor
         $('#sensor_name').html(option);
     });
 
+    //------------------------------------------------------------------
+    
+    /**
+     * Fungsi untuk mengambil data sensor berdasarkan nama sensor
+     * dan rentang waktu yang dipilih
+     * 
+     * @param  {string} sensor          [nama sensor yang dipilih]
+     * @param  {string} sensor_location [lokasi sensor yang dipilih]
+     * @param  {String} period          [periode atau rentang waktu]
+     * @return {Void}
+    */
     function getLeakTime(sensor, sensor_location, period = 'All the Time')
     {
+        // Variabel data untuk menampung data sensor dari firebase berupa array
         var data = [];
 
+        // Ambil data sensor berdasarkan nama sensor 
         database.ref('/'+sensor+'/').on('value', function (snap) {
 
             var sensor_data = snap.val();
 
-            // Get Sensor Data from Every Sensor Name 
+            // Ambil semua data dari setiap index berdasarkan sensor yang dipilih
             $.each(sensor_data, function (index, value) {
 
                 database.ref('/'+sensor+'/'+index).on('value', function (snapshot) {
                     
                     var sensor_data = snapshot.val();
 
+                    // Simpan pada variabel data dalam bentuk array
                     data.push(value);
                 })
             })
         })
         
-        var leaks = [];
+        // Data volume kebocoran
+        // var leaks = [];
+
+        // Data waktu kebocoran
         var timeleak = [];
 
+        // Format waktu yang digunakan
         const options = { 
             year: 'numeric', 
             month: 'long', 
@@ -142,39 +212,42 @@
             second:'numeric'
         };
 
+        // Pisahkan data waktu kebocoran
         for(var i in data[0]) timeleak.push(data[0][i]);
-        for(var i in data[1]) leaks.push(data[1][i]);
+        // for(var i in data[1]) leaks.push(data[1][i]);
 
         for (var i = 0; i < timeleak.length; i++) {
             var datetime = new Date(timeleak[i]); 
-            tableData.push({raw_time:timeleak[i],time:datetime.toLocaleDateString('id-ID', options),status:'Bocor',location:sensor_location,volume:leaks[i]});
+            tableData.push({
+                raw_time:timeleak[i],
+                time:datetime.toLocaleDateString('id-ID', options),
+                status:'Bocor',
+                location:sensor_location
+                // volume:leaks[i]
+            });
         }
-
-        // for(var a in data[4]) {
-        //     var datetime = new Date(data[4][a]); 
-        //     tableData.push({raw_time:data[4][a],time:datetime.toLocaleDateString('en-EN', options),status:'Ditambal',location:sensor_location,volume:'-'});
-        // }
 
         // Destroy Datatables Before Re-draw it to Renew Data
         $('#sensor_data_tbl').DataTable().destroy();
 
-        // Initialize Datatables
+        // Inisialisasi Datatables dan kirim data sensor ke datatables
         table = $('#sensor_data_tbl').DataTable({
 
-            "order": [[0,"desc"]],
+            "order": [[0,"desc"]], // Mengurutkan data berdasarkan kolom pertama
             'columnDefs': [ 
                 {
+                    // Menonaktifkan sorting pada kolom selain kolom pertama
                     'targets': [1,2,3],
                     'orderable': false,
                 },
                 { 
+                    // Mendeklarasikan tipe data date untuk kolom pertama
                     type: 'date', 
                     'targets': [0] 
                 }
             ],
-            // Parsing Sensor Data form Array to Datatables
+            // Masukkan data dari variable tableData ke datatables
             "data" : tableData,
-            // Enable Ordering by First Column (Datetime)
             columns : [
                 {
                     "data" : "raw_time",
@@ -191,15 +264,16 @@
                     "data" : "location",
                 }
             ],
-            // Enable Datatables Responsive
+            // Aktifkan fitur Datatables Responsive
             responsive: true,
-            // Datatables Button Configurations
+            // Konfigurasi Button Datatables
             dom: "<'row'<'col-sm-12 col-md-6'l>>" +
             "<'row'<'col-sm-12 mt-4 mb-2'Btr>>" +
             "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
             buttons: [
-            'copy',
-            'print',
+            'copy', // Aktifkan tombol copy
+            'print', // Aktifkan tombol print
+            // Aktifkan tombol export excel
             {
                 extend: 'excelHtml5',
                 pageSize: 'A4',
@@ -210,12 +284,14 @@
                     columns: [1,2,3],
                 }
             },
+            // Aktifkan tombol export csv
             {
                 extend: 'csv',
                 exportOptions: {
                     columns : [1,2,3]
                 }
             },
+            // Aktifkan tombol export pdf
             {
                 extend: 'pdfHtml5',
                 pageSize: 'A4',
@@ -249,15 +325,31 @@
         });
     }
 
-
+    /**
+     * 
+     * Fungsi event listener untuk menu pilih sensor
+     * Agar data berubah ketika pilihan menu sensor diubah
+     * 
+    */
     $('#sensor_name').on('change', function () {
         var sensorName = $('#sensor_name').val();
         var sensorLocation = $(this).find(':selected').data('location');
         $('.title').text('Sensor Data: '+ sensorName + ' ('+sensorLocation+')');
         tableData = [];
+
+        // Panggil fungsi getLeakTime(), kirim parameter nama dan lokasi sensor
         getLeakTime(sensorName, sensorLocation);
     })
+
+    ///------------------------------------------------------------------
     
+    /**
+     *
+     * Filter datatables untuk menampilkan data berdasarkan rentang waktu tertentu
+     * Buat event listener ketika tombol apply pada form daterange diclick, maka data
+     * akan ditampilkan sesuai dengan rentang waktu yang dipilih.
+     * 
+    */
     $('.applyBtn').on('click', function () {
 
         // Custom filtering function which will search data in column four between two values
@@ -298,7 +390,13 @@
         });
     })
 
-    // Get Sensor Name for Graph Indicators
+    //------------------------------------------------------------------
+    
+    /**
+     * 
+     * Ambil data status sensor untuk indikator sensor yang ditampilkan pada dashboard
+     * 
+    */
     database.ref('/').on('value', function (snapshot) {
 
         var value = snapshot.val();
@@ -311,18 +409,17 @@
 
         $.each(value, function (index, value) {
 
-            // Generate Sensor Indicators
+            // Menampilkan lampu sensor sesuai jumlah sensor pada firebase
             graph.push('<div class="led-box mb-4"><div id="'+index+'" class=""></div><p>'+index+'</p></div>');
-            
             $('.indicator').html(graph);
             
             sensors.push(index);
 
+            // Ambil data status sensor dari setiap sensor
             database.ref('/'+index+'/sensor_status').on('value', function (snap) {
 
                 if (value) {
-
-                    // Show led-red if leak_status is true, and led-green if false
+                    // Tampilkan warna merah jika leak_status true (bocor) dan hijau jika false (tidak bocor)
                     var color = ((value.sensor_status == true) ? 'led-red' : 'led-green');
                     colors.push(color);
                 }
@@ -335,12 +432,14 @@
             audioUp(sensors[i]+'-up');
             audioDown(sensors[i]+'-down');
 
+            // Jika warna sensor merah (bocor) maka aktifkan suara alarm 'danger'
             if(colors[i] === 'led-red') {
                 document.getElementById(sensors[i]+'-down').play();
                 document.getElementById(sensors[i]+'-down').muted = false;
                 document.getElementById(sensors[i]+'-up').pause();
                 document.getElementById(sensors[i]+'-up').muted = true;
             }
+            // Selain itu aktifkan alarm dering
             else
             {
                 document.getElementById(sensors[i]+'-up').play();
@@ -349,11 +448,13 @@
                 document.getElementById(sensors[i]+'-down').muted = true;
             }
 
-            // Change led color based on leak_status
+            // Ubah warna berdasarkan pada leak_status
             $('#'+sensors[i]).attr('class', colors[i]);
         }
     });
 
+    //------------------------------------------------------------------
+    
     // Get Browser Permission
     function getLocalStream() {
         navigator.mediaDevices.getUserMedia({
@@ -369,7 +470,9 @@
 
     getLocalStream();
 
-    // Get All Sensors for Chart 
+    //------------------------------------------------------------------
+    
+    // Ambil semua sensor untuk menampilkan chart
     database.ref('/').on('value', function (snapshot) {
 
         var value = snapshot.val();
@@ -380,7 +483,6 @@
 
         var count = [];
 
-        // Get Sensor Data from Every Sensor Name 
         $.each(value, function (index, value) { 
             
             sensorLists.push(index);
@@ -389,7 +491,7 @@
             
             var leakStatus = [];
 
-            // Get data where leak_status = true
+            // Ambil data leak_status = true
             database.ref('/'+index+'/leak_time').on('value', function (snap) {
 
                 var leak_data = snap.val();
@@ -399,27 +501,23 @@
                 $.each(leak_data, function (index_data, leak) {
 
                     if (leak) {
-
                         leaks.push(leak);
-                        const arrFiltered = leaks.filter(el => {
-                          return el != null && el != '';
-                        });
-
+                        // const arrFiltered = leaks.filter(el => {
+                        //   return el != null && el != '';
+                        // });
                     }
                 })
 
                 leakStatus.push(leaks.length);
-
-                // console.log(leaks);
             })
             
             chartData.push({name:index, data:[leakStatus]});
         });
 
-        // Convert Leak Counter Array to JSON
+        // Konversi Array Leak Counter menjadi JSON
         var jsonData = JSON.stringify(chartData);
 
-        // Begin Highchart
+        // Konfigurasi Highchart
         $(function () {
             var chartype = {
                 type: 'column'
@@ -472,9 +570,9 @@
         });
     });
 
-    // Show year in copyright
+    // Menampilkan tahun pada teks copyright
     var year = new Date().getFullYear();
     $('.year').text(year);
 
-    // Show dashboard version
+    // Menampilkan versi dashboard
     $('.version').text(VERSION);
